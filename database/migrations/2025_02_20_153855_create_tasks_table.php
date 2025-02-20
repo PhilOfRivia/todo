1cae953c89e0caad308209ec2b690e4cee8c701c
Phil <philrodcot@gmail.com>
2025-02-20 10:35:40 -0600

    primer commit

---
+++ database/migrations/2025_02_20_153855_create_tasks_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->dateTime('generated_at');
+            $table->dateTime('deadline');
+            $table->dateTime('compleated_at');
+            $table->foreignId('list_id');
+            $table->json('attachments');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
